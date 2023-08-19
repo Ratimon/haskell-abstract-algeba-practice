13a90d500cb3eac792d2c897990f4dba8e925fea
Ratimon <ratixoxo@gmail.com>
2023-08-19 09:45:27 +0700

    Feat (./): Adding Definition of a set#exercise

--- README.md
+++ README.md
@@ -8,5 +8,11 @@ cabal init
 
 ```bash
 cabal build
- cabal run abstract-algeba
+cabal run abstract-algeba
 ``` 
+
+## Problems
+
+1. Assume you have a proper definition for integers. Create a well-defined set of rational numbers.
+
+Express this `Q = { a / b : a ∈ Z, b ∈ Z, b ≠ 0 }` in Haskell
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
+data RationalNumber = RationalNumber Integer Integer
+
+-- Function to create a rational number
+createRational :: Integer -> Integer -> RationalNumber
+createRational p q
+    | q /= 0    = RationalNumber p q
+    | otherwise = error "Denominator cannot be zero"
+
+-- List comprehension to generate rational numbers
+rationalNumbers :: [RationalNumber]
+rationalNumbers = [ createRational p q | p <- [-1000..1000], q <- [-1000..1000], q /= 0 ]
+
+-- Function to convert a RationalNumber to a String
+rationalToString :: RationalNumber -> String
+rationalToString (RationalNumber p q) = show p ++ " / " ++ show q
+
+-- Main function with limited output
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    putStrLn "Generated Rational Numbers:"
+    let limitedResults = take 100 rationalNumbers
+    mapM_ (putStrLn . rationalToString) limitedResults
+
